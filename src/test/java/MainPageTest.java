7ca513e89a4a615d9b090207c75c5d69f2cfac05
Maksim <maksim.meleshkin@eikon.group>
2023-06-19 23:09:03 -0700

    changes

---
+++ src/test/java/MainPageTest.java
@@ -0,0 +1,11 @@
+import base.BaseTest;
+import org.testng.annotations.Test;
+
+public class MainPageTest extends BaseTest {
+
+    @Test
+    public void  newTest() {
+
+        openBaseUrl();
+    }
+}
