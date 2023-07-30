f4ea062998d0242b456f0be5a1806b60721807ad
BugorDmitriy <116024901+BugorDmitriy@users.noreply.github.com>
2023-07-30 00:14:34 -0400

    Add new test (#4)

--- src/test/java/tests/MainPageTest.java
+++ src/test/java/tests/MainPageTest.java
@@ -2,7 +2,9 @@ package tests;
 
 import base.BaseTest;
 import org.assertj.core.api.Assertions;
+import org.openqa.selenium.By;
 import org.testng.annotations.Test;
+import pages.MainPage;
 
 public class MainPageTest extends BaseTest {
 
@@ -12,5 +14,16 @@ public class MainPageTest extends BaseTest {
         String pageTitle = getDriver().getTitle();
 
         Assertions.assertThat(pageTitle).isEqualTo("99 Bottles of Beer | Start");
+    }
+
+    @Test
+    public void swichToBrowseLanguagesPageTest() {
+        MainPage mainPage = new MainPage(getDriver());
+        mainPage.clickBrowseLanguagesTab();
+
+        String pageTextContext = getDriver().findElement(By.tagName("body")).getText().trim();
+        String createdNewTitleName = "All languages starting with the letter A are shown, sorted by Language.";
+
+        Assertions.assertThat(pageTextContext).contains(createdNewTitleName);
     }
 }
