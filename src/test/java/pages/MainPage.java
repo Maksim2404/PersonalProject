7966721732a51d40c42e00b2203f055842e59eba
Bugor Dmitriy <bugord1983@gmail.com>
2023-07-30 00:08:39 -0400

    Add new test

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -2,9 +2,21 @@ package pages;
 
 import base.BasePage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 public class MainPage extends BasePage {
-    protected MainPage(WebDriver driver) {
+
+    @FindBy (xpath = "//li//a[text()='Browse Languages']")
+    private WebElement browseLanguagesTab;
+
+    public MainPage(WebDriver driver) {
         super(driver);
+    }
+
+    public MainPage clickBrowseLanguagesTab() {
+    click(browseLanguagesTab);
+
+    return this;
     }
 }
