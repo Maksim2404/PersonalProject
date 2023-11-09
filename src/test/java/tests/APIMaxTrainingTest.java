022fa5ec827a7bdf11c35a8763bbcabdacf9493f
Maksim2404 <makssamarskiy@icloud.com>
2023-11-09 09:07:41 -0800

    Added new JSON functionality for API test

--- src/test/java/tests/APIMaxTrainingTest.java
+++ src/test/java/tests/APIMaxTrainingTest.java
@@ -2,7 +2,11 @@ package tests;
 
 import base.BaseTest;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
 import org.testng.annotations.Test;
+import pages.APIPage;
+
+import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.given;
 
 public class APIMaxTrainingTest extends BaseTest {
@@ -11,32 +15,33 @@ public class APIMaxTrainingTest extends BaseTest {
     - given - all input details
     - when - submit the API - resource, http methods
     - then - validate the response
-    */
-    //first test - validate if API place API is working as expected
+    *//*
+    first test - validate if API place API is working as expected*/
     @Test
     public void addPlaceTest() {
 
         RestAssured.baseURI = "https://rahulshettyacademy.com";
-        given().log().all()
-                .queryParam("key", "qaclick123")
-                .header("Content-Type", "application/json")
-                .body("{\n" +
-                        "  \"location\": {\n" +
-                        "    \"lat\": -38.383494,\n" +
-                        "    \"lng\": 33.427362\n" +
-                        "  },\n" +
-                        "  \"accuracy\": 50,\n" +
-                        "  \"name\": \"Frontline house\",\n" +
-                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                        "  \"address\": \"29, side layout, cohen 09\",\n" +
-                        "  \"types\": [\n" +
-                        "    \"shoe park\",\n" +
-                        "    \"shop\"\n" +
-                        "  ],\n" +
-                        "  \"website\": \"http://google.com\",\n" +
-                        "  \"language\": \"French-IN\"\n" +
-                        "}")
-                .when().post("maps/api/place/add/json")
-                .then().log().all().assertThat().statusCode(200);
+        String response =
+                given().log().all()
+                        .queryParam("key", "qaclick123")
+                        .header("Content-Type", "application/json")
+                        .body(APIPage.addPlace())
+                        .when().post("maps/api/place/add/json")
+                        .then().assertThat().statusCode(200).body("scope", equalTo("APP"))
+                        .header("server", "Apache/2.4.52 (Ubuntu)")
+                        .extract().response().asString();
+
+        System.out.println(response);
+
+        /*For parsing JSON*/
+        JsonPath js = new JsonPath(response);
+        String placeId = js.getString("place_id");
+        System.out.println(placeId);
+    }
+
+    /*Add place -> Update Place with New Address -> Get place to validate if new address is present in response*/
+    @Test
+    public void updatePlaceWithNewAddressTest() {
+
     }
 }
