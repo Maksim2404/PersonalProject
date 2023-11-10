1bee42de2a4f107b0c308a7a2532a5fca254bdcd
Maksim2404 <makssamarskiy@icloud.com>
2023-11-09 21:21:40 -0800

    Added new classes for parsing JSON file

---
+++ src/test/java/pages/apii/ReUsableMethods.java
@@ -0,0 +1,17 @@
+package pages.apii;
+
+import base.BasePage;
+import io.restassured.path.json.JsonPath;
+import org.openqa.selenium.WebDriver;
+
+public class ReUsableMethods extends BasePage {
+    protected ReUsableMethods(WebDriver driver) {
+        super(driver);
+    }
+
+    public static JsonPath rawToJson(String response) {
+
+        JsonPath js1 = new JsonPath(response);
+        return js1;
+    }
+}
